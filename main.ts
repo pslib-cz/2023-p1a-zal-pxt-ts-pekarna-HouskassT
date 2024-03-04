1a8721ce12e1945118f19df1a1b07bd47def2322
HouskassT <147390757+HouskassT@users.noreply.github.com>
2024-03-04 17:08:08 +0100

    Hotovo

--- main.ts
+++ main.ts
@@ -11,19 +11,40 @@
 */
 type Odber = {
     cas: number, //čas je v sekundách
-    spotreba: number //spotřeba ve watech za hodinu
+    prikon: number //spotřeba ve watech za hodinu
+    prace: number
+    cena: number
 }
-const cena: number = 6.70; // Kč / kWh
+
+const cena_za_kwh: number = 6.70; // Kč za kWh
+
 let data: Array<Odber> = [
-    { cas: 600, spotreba: 3500 },
-    { cas: 120, spotreba: 0 },
-    { cas: 300, spotreba: 1700 }, // data[2].cas
-    { cas: 60, spotreba: 0 },
-    { cas: 800, spotreba: 1500 },
+    { cas: 600, prikon: 3500, prace: 0, cena: 0 },
+    { cas: 120, prikon: 0, prace: 0, cena: 0 },
+    { cas: 300, prikon: 1700, prace: 0, cena: 0 }, // data[2].cas
+    { cas: 60, prikon: 0, prace: 0, cena: 0 },
+    { cas: 800, prikon: 1500, prace: 0, cena: 0 },
 ]
 
 for (const peceme of data){
     console.logValue("čas: ", peceme.cas);
-    console.logValue("spotřeba: ", peceme.spotreba);
-    console.logValue("spotřebováno: ", peceme.spotreba / 3600 * peceme.cas);
+    console.logValue("spotřeba: ", peceme.prikon);
+    console.logValue("spotřebováno: ", peceme.prikon / 3600 * peceme.cas);
 }
+
+console.log("Spotřeba energie v jednotlivý časech:")
+console.log("------------------------------------")
+let energCelkem: number = 0
+let cenaCelkem: number = 0
+for (let i = 0; i <= 4; i++) {
+    data[i].prace = data[i].cas * data[i].prikon / 3600000000
+    data[i].cena = data[i].prace * cena_za_kwh
+    console.log(data[i].cas + "[ms] " + data[i].prikon.toString() + "[W] " +
+        (data[i].prace * 1000).toString() + "[Wh] " + data[i].cena.toString() + "[Kč]")
+    energCelkem += data[i].prace * 1000
+    cenaCelkem += data[i].cena
+}
+console.log("------------------------------------")
+console.log("Práce celkem: " + energCelkem + "[Wh]")
+console.log("Cena celkem:  " + cenaCelkem.toString() + "[Kč]")
+console.log("------------------------------------")
